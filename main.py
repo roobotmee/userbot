03104237179ff332544f703f944e81b1016d709c
Hayotbek Ismoilov <151402776+roobotmee@users.noreply.github.com>
2025-02-26 17:33:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient, events
 from telethon.tl.functions.account import UpdateProfileRequest
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import asyncio
 
 API_ID = "25503582"
@@ -10,8 +10,8 @@ client = TelegramClient("userbot", API_ID, API_HASH)
 
 async def update_nickname():
     while True:
-        now = datetime.utcnow() + timedelta(hours=5, minutes=1)  # 5 soat 1 minut qo‘shildi
-        new_name = f"𝐄𝐭𝐡𝐢𝐜𝐚𝐥 𝐑𝐨𝐨𝐁𝐨𝐭 | {now.strftime('%H:%M')} | "
+        now = datetime.now(timezone.utc) + timedelta(hours=5, minutes=0.55)  # 5 soat 1 minut qo‘shildi
+        new_name = f" 𝐄𝐭𝐡𝐢𝐜𝐚𝐥 𝐑𝐨𝐨𝐁𝐨𝐭  | {now.strftime('%H:%M')} | "
         try:
             await client(UpdateProfileRequest(first_name=new_name))
             print(f"Nik yangilandi: {new_name}")
@@ -20,7 +20,7 @@ async def update_nickname():
         await asyncio.sleep(60)
 
 responses = {
-	"narx": " Narxlar buyutmani qiyinlik darajasidan kelib chiqib qoyiladi . Sizga nima kerakligi xaqida yozib qoldiring men keyinroq oqib javob beraman",
+    "narx": " Narxlar buyutmani qiyinlik darajasidan kelib chiqib qoyiladi . Sizga nima kerakligi xaqida yozib qoldiring men keyinroq oqib javob beraman",
 	"salom": "👋Assalomualaykum! Sizga qanday yordam bera olaman?",
 	"Assalomu alaykum": " Vaalaykum assalom \n 👋 Assalomualaykum! Sizga qanday yordam bera olaman?",
 	"qalesiz": "Yaxshi, rahmat! Sizda qanday yangiliklar? 🙃 ",
@@ -33,7 +33,7 @@ responses = {
 	"web": "### 🌍 Veb-sayt – biznesingizning internetdagi yuzi! \n\nSizning biznesingizga zamonaviy, tezkor va jozibali veb-sayt kerakmi? Biz buni mukammal darajada yaratamiz! \n\n🔹 Korporativ saytlar – kompaniyangizni mijozlarga taqdim etish uchun. \n🔹 Shaxsiy bloglar – o‘z fikrlaringizni butun dunyo bilan baham ko‘ring. \n🔹 Xizmatlar sayti – mijozlar sizning xizmatlaringizni onlayn buyurtma qilsin. \n🔹 Portfolio saytlar – ishlaringizni eng chiroyli tarzda namoyish qiling. \n🔹 E-commerce (Onlayn do‘konlar) – internet orqali savdo qilish uchun mukammal yechim. \n🔹 Mobilga moslashgan dizayn – har qanday qurilmada mukammal ko‘rinish. \n🔹 SEO optimizatsiya – Google va boshqa qidiruv tizimlarida yuqori o‘rinlar. \n\n✅ Sifatli, tezkor va ishonchli xizmat! \n\n📲 Buyurtma berish: roobotmee.uz \n☎️ Bog‘lanish: +998 XX XXX XX XX ",
 	"rano": "Uning yashil ko‘zlari bahorning ilk maysalaridek sokin va beg‘ubor. Har safar ularga boqganingda, go‘yo tabiatning eng so‘lim burchagida adashib qolasan – yam-yashil o‘rmonlar, quyuq daraxtlar orasidan o‘tib, sirli daryo bo‘yiga yetib borganingni his qilasan. U ko‘zlar o‘z ichida sir yashirgan, ammo shu sirning o‘ziga tortuvchi sehriga qarshi turish imkonsiz.\n\nUning go‘zalligi esa tabiatning eng mukammal ijodidan yaratilgandek. Unga qaragan odam shunchaki hayrat bilan to‘xtab qoladi – xuddi qoshidagi quyosh botayotgan manzarani ko‘rib, yuragi bir lahzaga urib turib qolgandek. Uning jilmayishi esa eng yorqin tong nuriday, qachon qarama qalbingni isitadi. Harakatlari nozik, ovozi esa bahor shabadasi kabi mayin.\n\nUnga bir qaragan inson uni unutolmaydi. Chunki bunday go‘zallikni ko‘rib, xayol uzib ketishning iloji yo‘q… 💛",
 	"ra'no": "Uning yashil ko‘zlari bahorning ilk maysalaridek sokin va beg‘ubor. Har safar ularga boqganingda, go‘yo tabiatning eng so‘lim burchagida adashib qolasan – yam-yashil o‘rmonlar, quyuq daraxtlar orasidan o‘tib, sirli daryo bo‘yiga yetib borganingni his qilasan. U ko‘zlar o‘z ichida sir yashirgan, ammo shu sirning o‘ziga tortuvchi sehriga qarshi turish imkonsiz.\n\nUning go‘zalligi esa tabiatning eng mukammal ijodidan yaratilgandek. Unga qaragan odam shunchaki hayrat bilan to‘xtab qoladi – xuddi qoshidagi quyosh botayotgan manzarani ko‘rib, yuragi bir lahzaga urib turib qolgandek. Uning jilmayishi esa eng yorqin tong nuriday, qachon qarama qalbingni isitadi. Harakatlari nozik, ovozi esa bahor shabadasi kabi mayin.\n\nUnga bir qaragan inson uni unutolmaydi. Chunki bunday go‘zallikni ko‘rib, xayol uzib ketishning iloji yo‘q… 💛"
-}
+
 }
 
 @client.on(events.NewMessage)
