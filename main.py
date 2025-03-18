b6019dfa9842e708454be4ca056282d0a5d7380b
Hayotbek Ismoilov <151402776+roobotmee@users.noreply.github.com>
2025-03-18 21:21:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-
 from telethon import TelegramClient, events
 from telethon.tl.functions.account import UpdateProfileRequest
 from datetime import datetime, timedelta, timezone
 import asyncio
 
-API_ID = "25503582"
-API_HASH = "9bc607b4792e8276afa98b757b62e11e"
+API_ID = "20833513"
+API_HASH = "dc124b69d34bd7dd19cd7539769d27ef"
 
 client = TelegramClient("userbot", API_ID, API_HASH)
 
 async def update_nickname():
-	while True:
-		now = datetime.now(timezone.utc) + timedelta(hours=5, minutes=0.55)
-		new_name = f" ❮꯭❶꯭꯭꯭➣꯭ ᴢᴀʀɪғᴊᴏɴ ꯭꯭•꯭|꯭🖤  | {now.strftime('%H:%M')} "
-		try:
-			await client(UpdateProfileRequest(first_name=new_name))
-			print(f"Nik yangilandi: {new_name}")
-		except Exception as e:
-			print(f"Xatolik: {e}")
-		await asyncio.sleep(60)
+    while True:
+        now = datetime.now(timezone.utc) + timedelta(hours=5, minutes=0.55)  
+        new_name = f" ❮꯭❶꯭꯭꯭➣꯭ ᴢᴀʀɪғᴊᴏɴ ꯭꯭•꯭|꯭🖤  | {now.strftime('%H:%M')} | "
+        try:
+            await client(UpdateProfileRequest(first_name=new_name))
+            print(f"Nik yangilandi: {new_name}")
+        except Exception as e:
+            print(f"Xatolik: {e}")
+        await asyncio.sleep(60)
 
 responses = {
 "Qalesz":  "yaxshi rahmat ozingiz yaxshimi?",
@@ -34,51 +33,54 @@ responses = {
 "Tinchmi":  "da tinch ozidachi?",
 "qettasz":  "hozir band edim knroq yoza olasizmi?",
 "qettas":  "hoz azginca ishlarim bor knro yozvorin",
-"yaxshimisiz":  "yaxshi rahmat oziz yaxshimi?",
-"yaxshimisz":  "yaxshi rahmat nima yordam kerak?",
+"yaxshimisz":  "yaxshi rahmat oziz yaxshimi?",
+"yaxshimisiz":  "yaxshi rahmat nima yordam kerak?",
 "boshmisiz":  "yoq hozir ko'chada edim nima yordam kerak?",
 }
 
 @client.on(events.NewMessage)
 async def message_handler(event):
-	if event.is_private:
-		me = await client.get_me()
-		if event.sender_id != me.id:
-			text = event.raw_text.lower()
-			
-			if text.startswith('/del'):
-				await client.delete_messages(event.chat_id, [event.id])
-				return
-			
-			for question, answer in responses.items():
-				if text.strip() == question.lower():
-					await event.reply(answer)
-					break
+    if event.is_private:
+        me = await client.get_me()  
+        if event.sender_id != me.id:  
+            text = event.raw_text.lower()  
+            
+
+            if text.startswith('/del'):
+
+                await client.delete_messages(event.chat_id, [event.id])
+                return  
+            
+            for question, answer in responses.items():
+                if text.strip() == question.lower():  
+                    await event.reply(answer)
+                    break
 
 @client.on(events.NewMessage(pattern="/start"))
 async def start_handler(event):
-	if event.is_private:
-		await event.reply("Bot ishga tushdi!")
-
+    if event.is_private:
+        await event.reply("Bot ishga tushdi!")
+        
+        
 @client.on(events.NewMessage(pattern="/del"))
 async def delete_messages(event):
-	if event.is_private:
-		responses = []
-		async for message in client.iter_messages(event.chat_id, from_user=None):
-			responses.append(message)
-			try:
-				await client.delete_messages(event.chat_id, message.id, revoke=True)
-			except Exception as e:
-				print(f"Xabar o‘chirishda xatolik: {e}")
-		for response in responses:
-			try:
-				await client.delete_messages(event.chat_id, response.id, revoke=True)
-			except Exception as e:
-				print(f"Error ! < / > ")
-		await event.reply("")
+    if event.is_private:
+        responses = []
+        async for message in client.iter_messages(event.chat_id, from_user=None):
+            responses.append(message)
+            try:
+                await client.delete_messages(event.chat_id, message.id, revoke=True)
+            except Exception as e:
+                print(f"Xabar o‘chirishda xatolik: {e}")
+        for response in responses:
+            try:
+                await client.delete_messages(event.chat_id, response.id, revoke=True)
+            except Exception as e:
+                print(f"Error ! < / > ")
+        await event.reply("")
 
 print("Userbot ishga tushdi...")
 
 with client:
-	client.loop.create_task(update_nickname())
-	client.run_until_disconnected()
+    client.loop.create_task(update_nickname())
+    client.run_until_disconnected()
